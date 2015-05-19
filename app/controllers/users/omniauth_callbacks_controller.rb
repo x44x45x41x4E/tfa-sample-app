ce7a7604235c28401a0617fe3c388bd4f1eafebe
Dean Canlas <d.canlas07@gmail.com>
2015-05-19 13:51:00 +0800

    Add omniauth method for Etalio mobile connect provider

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
 
   def etalio
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Etalio") if is_navigational_format?
+    else
+      session["devise.etalio_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 
   # More info at:
