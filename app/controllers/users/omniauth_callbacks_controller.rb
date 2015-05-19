10b7539518756e258f18ca0e60f46af24cc7c99c
Dean Canlas <d.canlas07@gmail.com>
2015-05-19 13:26:36 +0800

    Rename scope for Devise controllers

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,14 @@
-class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
   # You should also create an action method in this controller like this:
   # def twitter
   # end
+
+  def etalio
+
+  end
 
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
